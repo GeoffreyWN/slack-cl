a320615b9a04239b580114488d326d6810b36eea
geof <>
2021-02-27 09:37:26 +0300

    initial commit, added react router

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD1MrNVFRLj8ubfct1N84braolMK6zcmUs",
+    authDomain: "slack-cl-6eac0.firebaseapp.com",
+    projectId: "slack-cl-6eac0",
+    storageBucket: "slack-cl-6eac0.appspot.com",
+    messagingSenderId: "214049339347",
+    appId: "1:214049339347:web:0cb18db5b1194d7a6c6126"
+  };
+  // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig)
